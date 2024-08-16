63f94983dcfaf34b0fb927db8e7d31520a4663e9
Nícolas <herozendeivol@gmail.com>
2024-08-16 09:12:47 -0300

    Atividade1

---
+++ Atividades.1/src/Questão8.java
@@ -0,0 +1,10 @@
+public class Questão8 {
+
+    public static void main(String[] args) {
+
+        String pala1 = "olá";
+        String pala2 = " mundo";
+
+        System.out.println(pala1 + pala2);
+    }
+}
