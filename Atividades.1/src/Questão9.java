48986b2f6dd5d5a29b6969b925de6bf60dece520
Nícolas <herozendeivol@gmail.com>
2024-08-16 09:48:29 -0300

    Atividade1

---
+++ Atividades.1/src/Questão9.java
@@ -0,0 +1,18 @@
+public class Questão9 {
+
+    public static void main(String[] args) {
+
+        int numero1 = 5;
+        int numero2 = 3;
+
+        int soma = numero1 + numero2;
+        int subtracao = numero1 - numero2;
+        int multiplicacao = numero1 * numero2;
+        int divisao = numero1 / numero2;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisão: " + divisao);
+    }
+}
