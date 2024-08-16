fc49dce885cec2f8dd7acf3ac299cbdc94187cca
Nícolas <herozendeivol@gmail.com>
2024-08-16 08:08:17 -0300

    Mensagem do commit descrevendo as mudanças

---
+++ Atividades/src/Questao2.java
@@ -0,0 +1,12 @@
+public class Questao2 {
+
+    public static void main(String[] args) {
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        System.out.println(numero1 + numero2);
+
+    }
+
+}
