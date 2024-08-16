07c87634fa57b086a4375b376424565aea66581c
Nícolas <herozendeivol@gmail.com>
2024-08-16 08:30:55 -0300

    Atividades1

---
+++ Atividades Lista 1/src/Questão2.java
@@ -0,0 +1,10 @@
+public class Questão2 {
+
+    public static void main(String[] args) {
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        System.out.print(numero1 + numero2);
+    }
+}
---
+++ Atividades Lista 1/src/Questão3.java
@@ -0,0 +1,9 @@
+public class Questão3 {
+
+    public static void main(String[] args) {
+
+        double variavel = 5.67;
+
+        System.out.println(variavel * 3);
+    }
+}
