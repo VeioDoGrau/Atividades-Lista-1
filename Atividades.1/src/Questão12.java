19ff00d197e229ce6d4852c7cc0a5e28ae047470
Nícolas <herozendeivol@gmail.com>
2024-08-19 08:28:33 -0300

    Atividades1

---
+++ Atividades.1/src/Questão12.java
@@ -0,0 +1,10 @@
+public class Questão12 {
+
+    public static void main(String[] args) {
+
+        int idadeAnos = 18;
+
+        idadeAnos = idadeAnos * 12;
+        System.out.println(idadeAnos);
+    }
+}
---
+++ Atividades.1/src/Questão13.java
@@ -0,0 +1,14 @@
+public class Questão13 {
+
+    public static void main(String[] args) {
+
+        int numero = 10;
+
+        if (numero >= 0 && numero <= 10) {
+            System.out.println("O número " + numero + " está entre 0 e 10.");
+        }
+        else {
+            System.out.println("O número " + numero + " não está entre 0 e 10.");
+        }
+    }
+}
