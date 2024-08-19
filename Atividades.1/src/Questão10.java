fcb0b7aafcc76946e0ad8778b2bf2dd4330a79b5
Nícolas <herozendeivol@gmail.com>
2024-08-19 08:09:14 -0300

    Atividades1

---
+++ Atividades.1/src/Questão10.java
@@ -0,0 +1,13 @@
+public class Questão10 {
+
+    public static void main(String[] args) {
+
+        double saldo = 500.50;
+
+        saldo = saldo + 300.25;
+
+        saldo = saldo - 150.75;
+
+        System.out.println(saldo);
+    }
+}
---
+++ Atividades.1/src/Questão11.java
@@ -0,0 +1,12 @@
+public class Questão11 {
+
+    public static void main(String[] args) {
+
+        String fruta1 = "Morango";
+        String fruta2 = "Goiaba";
+
+        String mensagem = "Eu gosto de " + fruta1 + " e " + fruta2 + ".";
+        
+        System.out.println(mensagem);
+    }
+}
