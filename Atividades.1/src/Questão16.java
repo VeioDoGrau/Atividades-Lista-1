632aba5d0eda1f13d08af6159d40ebdd5869ce8b
Nícolas <herozendeivol@gmail.com>
2024-08-19 09:58:39 -0300

    Atividades1

---
+++ Atividades.1/src/Questão16.java
@@ -0,0 +1,14 @@
+public class Questão16 {
+
+    public static void main(String[] args) {
+
+        int valor = 7;
+
+        if (valor % 3 == 0) {
+            System.out.println("O valor " + valor + " é múltiplo de 3.");
+        } else {
+            System.out.println("O valor " + valor + " não é múltiplo de 3.");
+
+        }
+    }
+}
